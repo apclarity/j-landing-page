c16130e50ed195ed47163398397b9aa16b0e6fd0
apclarity <andreaspeterc@gmail.com>
2022-11-11 16:45:50 +0700

    revisi detail expert

--- src/pages/Experts/DetailExpert/Index.vue
+++ src/pages/Experts/DetailExpert/Index.vue
@@ -74,13 +74,22 @@ const testimonies= [
         id: 1,
         img: new URL("../../../images/user-36-03.jpg", import.meta.url),
         name: "Malik Sulaiman",
-        desc: "Expert dapat menjelaskan materi dengan baik dan memberikan contoh yang relevan. Super fun!"
+        desc: "Expert dapat menjelaskan materi dengan baik dan memberikan contoh yang relevan. Super fun!",
+        rating: 3
     },
     {
         id: 2,
         img: new URL("../../../images/user-36-03.jpg", import.meta.url),
         name: "Nicholas Scratch",
-        desc: "Pelayanan tim Jobhun dilakukan dengan baik, dari mulai konfirmasi pembayaran sampai proses penjadwalan dan instruksinya. Bahkan saat kelas harus direschedule pun difollow up dengan baik. Good job team Jobhun! Sangat baik pelayanannya dan tujuan dari kelasnya. Expert yang mengajar juga top banget!"
+        desc: "Pelayanan tim Jobhun dilakukan dengan baik, dari mulai konfirmasi pembayaran sampai proses penjadwalan dan instruksinya. Bahkan saat kelas harus direschedule pun difollow up dengan baik. Good job team Jobhun! Sangat baik pelayanannya dan tujuan dari kelasnya. Expert yang mengajar juga top banget!",
+        rating: 5
+    },
+    {
+        id: 3,
+        img: new URL("../../../images/user-36-03.jpg", import.meta.url),
+        name: "Yth Pak Tono Suyono Mangkujiwo",
+        desc: "Pelayanan tim Jobhun dilakukan dengan baik, dari mulai konfirmasi pembayaran sampai proses penjadwalan dan instruksinya. Bahkan saat kelas harus direschedule pun difollow up dengan baik. Good job team Jobhun! Sangat baik pelayanannya dan tujuan dari kelasnya. Expert yang mengajar juga top banget!",
+        rating: 4
     }
 ]
 </script>
